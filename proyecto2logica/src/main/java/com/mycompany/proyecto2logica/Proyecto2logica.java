14c734c519d63a4e253a6d6221eaede812806727
camilo-avendano1 <81989221+camilo-avendano1@users.noreply.github.com>
2022-09-06 14:58:55 -0500

    Add files via upload

---
+++ proyecto2logica/src/main/java/com/mycompany/proyecto2logica/Proyecto2logica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyecto2logica;
+
+/**
+ *
+ * @author juanc
+ */
+public class Proyecto2logica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Worlda!");
+        String hilera = "(a,b,c,(d,e,(f,z),k,j),h,i)";  
+
+        System.out.println(String.valueOf(hilera.charAt(2)));
+    }
+}
